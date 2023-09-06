7660140603b86ce4a464739b500d87827a04d004
Gisela Rojas <gise.r.09@gmail.com>
2023-09-05 19:30:27 -0600

    3 tipos de variables

--- src/fundamentosjava1/FundamentosJava1.java
+++ src/fundamentosjava1/FundamentosJava1.java
@@ -7,7 +7,7 @@ package fundamentosjava1;
 
 /**
  *
- * @author giser
+ * @author Gisela Rojas
  */
 public class FundamentosJava1 {
 
@@ -15,7 +15,16 @@ public class FundamentosJava1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Crear 3 variables, una para la edad,
+        //otra para el nombre, y una ultima para 
+        //su salario
+        int edad = 32;
+        String nombre = "Gisela"; 
+        double salario = 300;
+        
+        //mostrar por pantalla Soy <nombre> tengo <edad> y mi sueldo es de <sueldo>
+        
+        System.out.println("Soy "+ nombre + ", tengo "+ edad + " años y mi salario es $"+ salario);
     }
     
 }
